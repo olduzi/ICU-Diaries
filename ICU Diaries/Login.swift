0c7424b06c256a71d5d3c57d52654782c7d22b3a
olduzi <olduzi@umich.edu>
2021-10-12 18:37:51 -0400

    login and logout

---
+++ ICU Diaries/Login.swift
@@ -0,0 +1,74 @@
+//
+//  Login.swift
+//  ICU Diary
+//
+//  Created by Simran  Chowdhry on 10/11/21.
+//
+
+import SwiftUI
+
+
+struct Login : View {
+    @State var username: String = ""
+    @State var password: String = ""
+    var body: some View {
+        VStack {
+            WelcomeText()
+            UserImage()
+            TextField("Username", text: $username)
+                            .padding()
+                            .background(Color(red: 0.98, green: 0.49, blue: 0.49))
+                            .cornerRadius(5.0)
+                            .padding(.bottom, 20)
+            SecureField("Password", text: $password)
+                            .padding()
+                            .background(Color(red: 0.98, green: 0.49, blue: 0.49))
+                            .cornerRadius(5.0)
+                            .padding(.bottom, 20)
+            Button(action: {print("Button tapped")}) {
+                           LoginButton()
+                        }
+        }
+        .padding()
+    }
+}
+
+struct WelcomeText: View {
+    var body: some View {
+        VStack {
+            Text("Welcome!")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .padding(.bottom, 20)
+        }
+    }
+}
+
+struct UserImage : View {
+    var body: some View {
+        VStack {
+            return Image("userImage")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 150, height: 150)
+                .clipped()
+                .cornerRadius(150)
+                .padding(.bottom, 75)
+        }
+    }
+}
+
+struct LoginButton : View {
+    var body: some View {
+        VStack {
+            Text("SIGN IN")
+                .font(.headline)
+                .foregroundColor(.white)
+                .padding()
+                .frame(width: 220, height: 60)
+                .background(Color.green)
+                .cornerRadius(15.0)
+        }
+        .padding()
+    }
+}
---
+++ ICU Diaries/Logout.swift
@@ -0,0 +1,86 @@
+//
+//  Logout.swift
+//  ICU Diary
+//
+//  Created by Simran  Chowdhry on 10/11/21.
+//
+
+import SwiftUI
+
+struct LogOut: View {
+    var body: some View {
+        VStack {
+            ICUDiaryText()
+            LogoutQuestionText()
+            NavigationLink(destination: Login()) {
+                                Text("Awesome Button")
+                                .frame(minWidth: 0, maxWidth: 300)
+                                .padding()
+                                .foregroundColor(.white)
+                                .background(LinearGradient(gradient: Gradient(colors: [Color.red, Color.blue]), startPoint: .leading, endPoint: .trailing))
+                                .cornerRadius(40)
+                                .font(.title)
+            }
+            Button(action: {print("Button tapped")}) {
+                           LogoutYesButton()
+                        }
+            Button(action: {print("Button tapped")}) {
+                           LogoutNoButton()
+                        }
+        }
+    }
+}
+
+
+struct ICUDiaryText: View {
+    var body: some View {
+        VStack {
+            Text("ICU Diary")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding(.bottom, 20)
+        }
+    }
+}
+
+
+struct LogoutQuestionText: View {
+    var body: some View {
+        VStack {
+            Text("Are you sure you want to logout?")
+                .fontWeight(.semibold)
+                .padding(.bottom, 20)
+        }
+    }
+}
+
+struct LogoutYesButton : View {
+    var body: some View {
+        VStack {
+            Text("YES")
+                .font(.headline)
+                .foregroundColor(.white)
+                .padding()
+                .frame(width: 220, height: 60)
+                .background(Color.green)
+                .cornerRadius(15.0)
+        }
+        .padding()
+    }
+}
+
+
+struct LogoutNoButton : View {
+    var body: some View {
+        VStack {
+            Text("NO")
+                .font(.headline)
+                .foregroundColor(.white)
+                .padding()
+                .frame(width: 220, height: 60)
+                .background(Color.green)
+                .cornerRadius(15.0)
+        }
+        .padding()
+    }
+}
