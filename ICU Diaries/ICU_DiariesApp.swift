0693cbcf7b1fb37ec012b54afa6300e668af5226
olduzi <olduzi@umich.edu>
2021-10-12 18:21:13 -0400

    Initial Commit

---
+++ ICU Diaries/ICU_DiariesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ICU_DiariesApp.swift
+//  ICU Diaries
+//
+//  Created by Olduz Ilkhchoui on 10/12/21.
+//
+
+import SwiftUI
+
+@main
+struct ICU_DiariesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
