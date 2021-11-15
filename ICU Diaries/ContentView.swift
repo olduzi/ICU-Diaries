42852694f0725912f79f646f272447e6290632b6
olduzi <olduzi@umich.edu>
2021-11-14 22:56:00 -0500

    preview now works

--- ICU Diaries/ContentView.swift
+++ ICU Diaries/ContentView.swift
@@ -1,30 +1,31 @@
+////
+////  ContentView.swift
+////  ICU Diaries
+////
+////  Created by Olduz Ilkhchoui on 10/12/21.
+////
 //
-//  ContentView.swift
-//  ICU Diaries
+//import SwiftUI
 //
-//  Created by Olduz Ilkhchoui on 10/12/21.
+////example
+//struct ContentView: View {
 //
-
-import SwiftUI
-
-//example
-struct ContentView: View {
-    
-    @State var entries = Entry(sender: "olduzi", reciever: "olduzi", title: "Test Post", content: "Successful Post")
-    @State var content = "hi"
-    
-    var body: some View {
-        Text("\(content)")
-            .onAppear() {
-                GetDiary().sendData(entry: entries) { (response) in
-                    self.content = response
-                }
-            }.navigationTitle("Quote List")
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+//    @State var entries = Entry(sender: "olduzi", receiver: "olduzi", title: "Test Post", content: "Successful Post")
+//    @State var content = "hi"
+//
+//    var body: some View {
+//        Text("\(content)")
+//            .navigationTitle("Quote List")
+//            .onAppear() {
+//                GetDiary().sendData(entry: entries) { (response) in
+//                    self.content = response
+//                }
+//            }
+//    }
+//}
+//
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
